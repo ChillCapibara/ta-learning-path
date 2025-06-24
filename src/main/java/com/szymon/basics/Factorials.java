73e37ef85204a910e9c8bcea443df0deda655dc7
LazyCapibara <szymen38@gmail.com>
2025-06-24 22:29:33 +0200

    init: create a project with java basics

---
+++ src/main/java/com/szymon/basics/Factorials.java
@@ -0,0 +1,10 @@
+package com.szymon.basics;
+
+public class Factorials {
+
+    public static void main(String[] args) {
+        System.out.println("works");
+    }
+
+
+}
---
+++ src/main/java/com/szymon/basics/ReverseString.java
@@ -0,0 +1,4 @@
+package com.szymon.basics;
+
+public class ReverseString {
+}
