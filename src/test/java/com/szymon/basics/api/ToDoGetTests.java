2ac1625e8a545dfdc09eb8120cec52adcda9ea3d
LazyCapibara <szymen38@gmail.com>
2025-07-21 21:16:47 +0200

    POST tests, and refactoring

--- src/test/java/com/szymon/basics/api/ToDoGetTests.java
+++ src/test/java/com/szymon/basics/api/ToDoGetTests.java
@@ -5,16 +5,21 @@ import com.szymon.common.JsonUtils;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ToDoGetTests {
 
-private final String baseURI = "https://dummyjson.com";
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://dummyjson.com";
+    }
 
     @Test
-    public void verifyStatusCode(){
+    public void verifyStatusCode() {
         RestAssured
-                .get(baseURI + "/todos/1")
+                .get("/todos/1")
                 .then()
                 .assertThat()
                 .statusCode(200);
@@ -22,19 +27,19 @@ private final String baseURI = "https://dummyjson.com";
 
     @Test
     public void verifyResponseBody() throws Exception {
-        Response response = RestAssured.get(baseURI + "/todos/1");
+        Response response = RestAssured.get("/todos/1");
         JsonNode actualResults = JsonUtils.convertResponseIntoJson(response);
-        JsonNode expectedResponse = JsonUtils.loadJsonFromResources("api/dummy/expectedResponseGetToDo.json");
+        JsonNode expectedResponse = JsonUtils.loadJsonFromResources("api/dummy/responses/expectedResponseGetToDo.json");
 
         Assert.assertEquals(actualResults, expectedResponse);
     }
 
     @Test
     public void verifyIdempotenceOfResponse() throws Exception {
-        Response response1 = RestAssured.get(baseURI + "/todos/1");
+        Response response1 = RestAssured.get("/todos/1");
         JsonNode response1Json = JsonUtils.convertResponseIntoJson(response1);
 
-        Response response2 = RestAssured.get(baseURI + "/todos/1");
+        Response response2 = RestAssured.get("/todos/1");
         JsonNode response2Json = JsonUtils.convertResponseIntoJson(response2);
 
         Assert.assertEquals(response1Json, response2Json);
---
+++ src/test/java/com/szymon/basics/api/ToDoPostTests.java
@@ -0,0 +1,117 @@
+package com.szymon.basics.api;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.szymon.common.JsonUtils;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ToDoPostTests {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://dummyjson.com";
+    }
+
+    @Test
+    public void toDoCanBeCreated() throws Exception {
+        JsonNode requestBody = JsonUtils.loadJsonFromResources("api/dummy/requests/toDoCreate.json");
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(requestBody.toString())
+                .when()
+                .post("/todos/add")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(response.statusCode(), 201, "Invalid status code");
+
+        Assert.assertTrue(response.jsonPath().get("id") instanceof Integer);
+        Assert.assertTrue(response.jsonPath().getInt("id") > 0, "Id not assigned");
+
+        Assert.assertTrue(response.jsonPath().get("completed") instanceof String);
+        Assert.assertEquals(response.jsonPath().getString("completed"), "false", "Invalid completion status");
+
+        Assert.assertTrue(response.jsonPath().get("userId") instanceof Integer);
+        Assert.assertEquals(response.jsonPath().getInt("userId"), 99, "Invalid userId");
+    }
+
+
+    @DataProvider(name = "invalidUserIdFields")
+    public Object[][] invalidBodyUserIdField() {
+        return new Object[][]{
+                {"lorem"},
+                {true},
+                {"lorem !@#$%^&*()_+|}{\n\t'\";,./<>?"}
+        };
+    }
+
+    @Test(dataProvider = "invalidUserIdFields")
+    public void invalidUserIdBody(Object val) throws Exception {
+        ObjectNode payload = (ObjectNode) JsonUtils.loadJsonFromResources("api/dummy/requests/toDoCreate.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode userIdNode = mapper.valueToTree(val);
+        payload.set("userId", userIdNode);
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(payload.toString())
+                .when()
+                .post("/todos/add")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(response.statusCode(), 400, "Invalid status code");
+        Assert.assertEquals(response.jsonPath().getString("message"), String.format("Invalid user id '%s'", val));
+    }
+
+
+    @Test
+    public void nullUserIdField() throws Exception {
+        ObjectNode payload = (ObjectNode) JsonUtils.loadJsonFromResources("api/dummy/requests/toDoCreate.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode userIdNode = mapper.valueToTree(null);
+        payload.set("userId", userIdNode);
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(payload.toString())
+                .when()
+                .post("/todos/add")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(response.statusCode(), 400, "Invalid status code");
+        Assert.assertEquals(response.jsonPath().getString("message"), "User id is required");
+    }
+
+
+    @Test
+    public void nonExistingUser() throws Exception {
+        ObjectNode payload = (ObjectNode) JsonUtils.loadJsonFromResources("api/dummy/requests/toDoCreate.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode userIdNode = mapper.valueToTree("\n");
+        payload.set("userId", userIdNode);
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(payload.toString())
+                .when()
+                .post("/todos/add")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(response.statusCode(), 404, "Invalid status code");
+        Assert.assertEquals(response.jsonPath().getString("message"), "User with id '\n' not found");
+    }
+
+
+}
