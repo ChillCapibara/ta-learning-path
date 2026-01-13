de500e11c2e1b9721ace987b8e1e9759925e911b
LazyCapibara <szymen38@gmail.com>
2026-01-13 21:27:12 +0100

    feature: configured allure reporting, TestNG listener screenshots, updated MDC cleanup, updated webdrivermanager and logback versions

--- framework-web/src/test/java/framework/listeners/MdcTestListener.java
+++ framework-web/src/test/java/framework/listeners/MdcTestListener.java
@@ -1,9 +1,17 @@
 package framework.listeners;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import java.io.ByteArrayInputStream;
 
 public class MdcTestListener implements ITestListener {
 
@@ -16,21 +24,32 @@ public class MdcTestListener implements ITestListener {
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        MDC.clear();
+        MDC.remove("test");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        MDC.clear();
+        WebDriver driver = framework.driver.WebDriverManager.getDriver();
+        if (driver != null) {
+            try {
+                byte[] png = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                Allure.attachment("Screenshot - " + result.getName(),
+                        new ByteArrayInputStream(png));
+            } catch (Exception e) {
+                Logger log = LoggerFactory.getLogger(MdcTestListener.class);
+                log.warn("Failed to capture screenshot for {}: {}", result.getName(), e.toString());
+            }
+        }
+        MDC.remove("test");
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        MDC.clear();
+        MDC.remove("test");
     }
 
     @Override
     public void onFinish(ITestContext context) {
-        MDC.clear();
+        MDC.remove("test");
     }
 }
